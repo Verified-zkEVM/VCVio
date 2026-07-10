0914af1bb91887faadf4d34bdc5cc1a7e5ceb6ff
Abraxas1010 <rgoodman@apoth3osis.io>
2026-07-10 10:40:13 -0400

    style(MerkleTree): shorten batch module copyright headers to the line-length limit

--- VCVio/CryptoFoundations/MerkleTree/Inductive/BatchDefs.lean
+++ VCVio/CryptoFoundations/MerkleTree/Inductive/BatchDefs.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2026 The Institute for Ontological Mathematics / Equation Capital dba Apoth3osis. All rights reserved.
+Copyright (c) 2026 IAOM / Equation Capital dba Apoth3osis. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Abraxas1010 (IAOM / Apoth3osis)
 -/
