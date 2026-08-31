740248b2c6b957d2ddfbd1c1e01f746471e3eba7
Quang Dao <qvd@andrew.cmu.edu>
2026-08-31 13:35:54 -0700

    feat(slhdsa): connect WOTS traces to target ledgers

--- HashSig.lean
+++ HashSig.lean
@@ -38,6 +38,7 @@ public import HashSig.SLHDSA.Security.GeneralScheme
 public import HashSig.SLHDSA.Security.Notions
 public import HashSig.SLHDSA.Security.OracleSurface
 public import HashSig.SLHDSA.Security.ReachableTargets
+public import HashSig.SLHDSA.Security.TraceTargets
 public import HashSig.SLHDSA.Security.Transcript
 public import HashSig.SLHDSA.Wots
 public import HashSig.SLHDSA.WotsChecksum
